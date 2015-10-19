230ce2bcb966ad865b76c6cf9e4bb07aa3e296af
Paweł Szulc <paul.szulc@gmail.com>
2015-10-20 00:01:34 +0200

    Add ch4 safetail implementations

--- ch04.hs
+++ ch04.hs
@@ -2,4 +2,15 @@
 -- splitAt n xs  = (take n xs, drop n xs)        
 
 halve :: [a] -> ([a],[a])
-halve xs = splitAt ((length xs) `div` 2) xs      
+halve xs = splitAt ((length xs) `div` 2) xs
+
+safetail1 :: [a] -> [a]
+safetail1 xs = if (length xs) > 0 then drop 1 xs else xs          
+
+safetail2 :: [a] -> [a]
+safetail2 xs | (length xs) > 0 = drop 1 xs
+             | otherwise = xs
+
+safetail3 :: [a] -> [a]
+safetail3 [] = []
+safetail3 (x:xs) = xs
