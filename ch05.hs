e31f5f0087da9430cbfcbaae0e39bb4c2dd2a0dc
Paweł Szulc <paul.szulc@gmail.com>
2015-10-22 00:49:28 +0200

    Add ch5 ex3

--- ch05.hs
+++ ch05.hs
@@ -2,3 +2,6 @@ hundredSquares = sum [x ^ 2 | x <- [1..100]]
 
 replicate' :: Int -> a -> [a]
 replicate' n a = [a | x <- [1..n]] 
+
+pythagorean :: Int -> [(Int, Int, Int)]
+pythagorean n = [(x,y,z) | x <- [1..n], y <- [1..n], z <- [1..n], (x^2) + (y ^ 2) == (z^2) ]
